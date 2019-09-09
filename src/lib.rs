507330e42bb4b185c527981d641668e2a327e9b0
linyows <linyows@gmail.com>
2019-09-09 13:39:57 +0900

    fix wrong path

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,7 @@ use http_guest::{Request, Response, RequestExt};
 
 pub fn user_entrypoint(_req: &Request<Vec<u8>>) -> Response<Vec<u8>> {
     let mut vec: Vec<u8> = Vec::new();
-    let mut url = "https://raw.githubusercontent.com/linyows/octopass/master/misc/kagoshima.jpg";
+    let mut url = "https://raw.githubusercontent.com/linyows/rutouch-terrarium/master/misc/kagoshima.jpg";
     let req = Request::builder()
         .method("GET")
         .uri(url)
